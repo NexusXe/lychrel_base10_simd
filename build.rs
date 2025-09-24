d38fc293975af8f1560729c7ffa3920d6a335c2f
NexusXe <andastrike@gmail.com>
2025-09-24 09:33:40 -0500

    only run windows build script for windows builds

--- build.rs
+++ build.rs
@@ -1,7 +1,9 @@
 fn main() {
+    #[cfg(target_family = "windows")] {
     // This script runs before the main compilation.
     // It compiles the manifest and tells rustc to link it.
     embed_resource::compile("lychrel_base10_simd-manifest.rc", embed_resource::NONE)
         .manifest_optional()
         .unwrap();
+    }
 }
