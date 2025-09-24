46a66b05843aeb59aa793c7ceacce2ccf4c90123
NexusXe <andastrike@gmail.com>
2025-09-24 09:20:34 -0500

    compile for environment newer than Vista

---
+++ build.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // This script runs before the main compilation.
+    // It compiles the manifest and tells rustc to link it.
+    embed_resource::compile("lychrel_base10_simd-manifest.rc", embed_resource::NONE)
+        .manifest_optional()
+        .unwrap();
+}
