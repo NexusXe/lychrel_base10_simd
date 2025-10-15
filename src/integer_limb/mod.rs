b9c864bcc0b7dc2956fe542e08ca1f9a8267f9d9
NexusXe <andastrike@gmail.com>
2025-10-15 12:46:24 -0500

    further optimize prefetch asm

--- src/integer_limb/mod.rs
+++ src/integer_limb/mod.rs
@@ -629,21 +629,18 @@ impl<T: Allocator + Clone + Copy> Integer<T> {
         #[allow(clippy::pointers_in_nomem_asm_block)] // ptr is being used for prefetch
         unsafe {
             // prefetch the first 64 limbs since, for integers larger than L3$, they've probably been evicted by now
-            const L1C: usize = 16; // 1024 bytes into 48 KiB L1d$ w/ intent to write
             // prefetching in asm because I don't want this loop unrolled
             // while it probably doesn't matter, less pollution in the L1i$ and the L1$ overall is good
             // asm version uses 12 bytes overall, whereas unrolled version was 7 * 16 = 112 bytes
             std::arch::asm!(r#"
-            # implicit xor eax, eax; 2 bytes
+            # implicit xor {i:e}, {i:e}; 2 bytes, 1 uop
             2:
-            prefetchw byte ptr [{limbs_ptr} + rax * 8] # 4 bytes
-            add al, 8 # 2 bytes
-            cmp al, {LIMIT} # 2 bytes
-            jne 2b # 2 bytes
+            prefetchw byte ptr [{limbs_ptr} + {i:r} * 8] # 4 bytes, 1 uop
+            add {i:l}, 8 # 3 or 4 bytes; fuses with conditional jump for 1 uop for both
+            jns 2b # 2 bytes; shares uop with add instruction
             "#,
             limbs_ptr = in(reg) limbs_ptr,
-            in("rax") 0, // use rax so the add and cmp insns are only 2 bytes
-            LIMIT = const L1C * 8,
+            i = in(reg) 0,
             options(nostack, nomem));
 
             // in addition to the first limbs, the last ones are also accessed first
