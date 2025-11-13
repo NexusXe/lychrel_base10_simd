077ee9e56a3cfde9a94c62539a25c9900fea7c18
NexusXe <andastrike@gmail.com>
2025-11-13 12:24:26 -0600

    more build optimizations

--- src/integer_limb/mod.rs
+++ src/integer_limb/mod.rs
@@ -435,6 +435,12 @@ impl std::ops::Add for Limb {
 
     #[inline(always)]
     fn add(self, other: Self) -> Self::Output {
+        if self.0 & LimbVec::splat(0xF0) != LimbVec::splat(0)
+            || other.0 & LimbVec::splat(0xF0) != LimbVec::splat(0)
+        {
+            impossible!("Tried to wide add Limbs with dirty uppers");
+        }
+
         unsafe {
             let input_64: WideVec = transmute(self.0);
             let other_64: WideVec = transmute(other.0);
@@ -638,9 +644,9 @@ impl<T: Allocator + Clone + Copy> Integer<T> {
                     // 2: 26.2
                     // 3: 24.5
                     // 4: 25.3
-                    for _ in 0..3 {
-                        carry_mask |= ng_carry_mask & (carry_mask << 1);
-                    }
+                        for _ in 0..3 {
+                            carry_mask |= ng_carry_mask & (carry_mask << 1);
+                        }
 
                     if likely(carry_mask != 0) || forward_carry {
                         ever_carried = true;
